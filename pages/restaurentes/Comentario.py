ebaebd87ca220b1e3cdd23ec04cc8e9eedfaf7ac
Gabriel Corrêa <gabriel@correa.dev.br>
2022-04-11 20:10:27 -0300

    feature: adicionar comentarios a um restaurnte

---
+++ pages/restaurentes/Comentario.py
@@ -0,0 +1,5 @@
+class Comentario:
+    def __init__(self, id, nome, comentario):
+        self.id = id
+        self.comentario = comentario
+        self.nome = nome
--- pages/restaurentes/Restaurante.py
+++ pages/restaurentes/Restaurante.py
@@ -1,8 +1,9 @@
 class Restaurante:
-    def __init__(self, id, nome, imagem, endereco, horarioFuncionamento):
+    def __init__(self, id, nome, imagem, endereco, horarioFuncionamento, comentarios):
         self.id = id
         self.nome = nome
         self.imagem = imagem
         self.endereco = endereco
         self.horarioFuncionamento = horarioFuncionamento
+        self.comentarios = comentarios
         
